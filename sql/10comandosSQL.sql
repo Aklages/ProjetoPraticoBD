537cd707558640a6502a88540e254900261428ea
Andre <ak1lages@gmail.com>
2025-11-20 18:01:45 -0300

    template ex

---
+++ sql/10comandosSQL.sql
@@ -0,0 +1,12 @@
+a) junções de 3 ou mais tabelas, com ORDER BY
+b) junções de 3 ou mais tabelas, com ORDER BY e filtros na cláusula WHERE
+c) junção de 3 ou mais tabelas, usando os operadores LIKE e BETWEEN
+d) junção de 3 ou mais tabelas, usando os operadores IN e IS NULL/IS NOT NULL
+e) junção de 2 ou mais tabelas com GROUP BY, sem HAVING, usando uma função agregada
+qualquer (MIN, MAX, AVG, SUM, COUNT). Use ORDER BY
+f) junção de 2 ou mais tabelas com GROUP BY e HAVING, usando uma função agregada
+qualquer (MIN, MAX, AVG, SUM, COUNT)
+g) subselect sem correlação
+h) subselect com correlação
+i) subselect com EXISTS
+j) junção de 2 ou mais tabelas com GROUP BY e HAVING, usando uma função agregada qualquer (MIN, MAX, AVG, SUM, COUNT)
